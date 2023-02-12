2db8c0707498fbef87063c8eac338312201a44d5
vbynd <vbynd0@gmail.com>
2023-02-12 19:23:54 +0400

    Initial commit

---
+++ src/test/java/demoqa/BaseTest.java
@@ -0,0 +1,24 @@
+package demoqa;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        this.driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        this.driver.quit();
+    }
+}
