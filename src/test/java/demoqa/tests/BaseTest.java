bd8550b41cc444402e9ae54097d24eb74d641077
vbynd <vbynd0@gmail.com>
2023-03-08 12:25:09 +0400

    Changed Chrome driver options

--- src/test/java/demoqa/tests/BaseTest.java
+++ src/test/java/demoqa/tests/BaseTest.java
@@ -19,7 +19,6 @@ public class BaseTest {
         options.addArguments("start-maximized"); // open Browser in maximized mode
         options.addArguments("disable-infobars"); // disabling infobars
         options.addArguments("--disable-extensions"); // disabling extensions
-        options.addArguments("--disable-gpu"); // applicable to windows os only
         options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
         options.addArguments("--no-sandbox"); // Bypass OS security model
         this.driver = WebDriverManager.chromedriver().capabilities(options).create();
